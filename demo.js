b74604dd59e989f6e7e38f45005598f5f9fabbd2
Nathan Rajlich <nathan@tootallnate.net>
2010-09-09 23:05:58 -0700

    A little reorganization. Also added a another radio station to test from (with 3 variations!).

--- demo.js
+++ demo.js
@@ -1,31 +1,38 @@
-var radio = require("./radio");
+var radio = require("./lib/radio-stream");
 
 //var url = "http://66.197.229.245:9882";
 // 4 Ever Floyd
 var url = "http://67.205.85.183:7714";
 // KFRC
 //var url = "http://2133.live.streamtheworld.com:80/KFRCFMCMP3";
+// .977 The Comedy Channel - MP3 / 48k
+//var url = "http://icecast1.977music.com/comedy";
+// .977 The Comedy Channel - MP3 / 80k
+//var url = "http://icecast2.977music.com/comedy";
+// .977 The Comedy Channel - AAC / 48k
+//var url = "http://icecast3.977music.com/comedy";
+
 var stream = radio.createReadStream(url);
   
 // TEMPORARY
-//stream.connection = require("fs").createReadStream("4everFloyd.mp3");
-//stream.metaint = 8192;
-//stream.connection = require("fs").createReadStream("KFRC-16000.mp3");
-//stream.metaint = 16000;
-//stream.bindedOnData = stream.onData.bind(stream);
-//stream.connection.addListener("data", stream.bindedOnData);
-
+function testFile(filename, metaint) {
+  stream.connection.destroy();
+  stream.connection = require("fs").createReadStream(filename);
+  stream.connection.on("data", stream.bindedOnData);
+  stream.metaint = metaint;
+}
+//testFile(__dirname + "/tests/dumps/TheComedyChannel-1-16000.mp3", 16000);
 
 stream.on("connect", function() {
-  //console.error("Radio Stream connected!");
-  //console.error(stream.headers);
+  console.error("Radio Stream connected!");
+  console.error(stream.headers);
 });
 
 var data=0;
 stream.on("data", function(chunk) {
-  for (var i=0, l=chunk.length; i<l; i++) {
+  //for (var i=0, l=chunk.length; i<l; i++) {
     //console.error((++data) + ": " + chunk[i]);
-  }
+  //}
   process.stdout.write(chunk);
 });
 
