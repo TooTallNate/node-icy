52920a315e84927635b8a6d5490c49f0ba4fcc75
Nathan Rajlich <nathan@tootallnate.net>
2010-09-09 23:09:32 -0700

    Rewrote the synchronous stream parser proof-of-concept. Extracting metadata works fine but the output data still sounds weird. Still looking into it...

--- tests/file.js
+++ tests/file.js
@@ -1,25 +1,38 @@
 var fs = require("fs");
-var metaint = 16000;
+const endOfHeader = "\r\n\r\n";
 
-a = fs.readFileSync("KFRC-16000.mp3");
+var file = __dirname + "/dumps/TheComedyChannel-1-16000.mp3";
+var dash = file.lastIndexOf("-")+1;
+var metaint = Number(file.substring(dash, file.indexOf(".", dash)));
+
+var a = fs.readFileSync(file);
 var bytesRead = 0;
 var metaLength;
-for (var i=0, l=a.length; i<l; i++) {
-  var b = a[i];
-  bytesRead++;
-  if (bytesRead == (metaint+1)) {
-    metaLength = b*16;
-    if (metaLength) {
-      console.error("MetaLength: " + b+"*16= " + metaLength);
-      var meta = a.slice(i+1, i+metaLength);
-      console.error(meta.toString());
-      i+=metaLength;
-    } else {
-      console.error("No MetaData!");
-    }
-    bytesRead = 0;
+
+var firstByte = a.slice(0, 600).toString().indexOf(endOfHeader) + endOfHeader.length;
+var headerBuf = a.slice(0, firstByte);
+var header = headerBuf.toString('ascii');
+a = a.slice(firstByte, a.length);
+//console.error(headerBuf);
+console.error(header);
+//console.error(header.length == headerBuf.length);
+
+while (a.length) {
+  var b = a.slice(0, Math.min(metaint-1, a.length));
+  process.stdout.write(b);
+  if (b.length === a.length) break;
+  
+  a=a.slice(metaint, a.length);
+  
+  metaLength = a[0] * 16;
+  console.error(metaLength);
+  a=a.slice(1, a.length);
+  if (metaLength) {
+    //console.error("MetaLength: " + b+"*16= " + metaLength);
+    var meta = a.slice(0, metaLength-1);
+    console.error(meta.toString());
+    a=a.slice(metaLength, a.length);
   } else {
-    //console.error((bytesRead-1) +": " +b);
+    console.error("No MetaData!");
   }
-  //console.error((bytesRead-1) +": " +b);
 }
