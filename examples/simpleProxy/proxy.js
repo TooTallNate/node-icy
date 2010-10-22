b517da6ce36b1ae308a372bfae2ce9f06c80a59f
Nathan Rajlich <nathan@tootallnate.net>
2010-10-21 18:53:26 -0700

    Made the 'simpleProxy' example less simple in an attempt to make the 'index.html' page more cross-browser.

--- examples/simpleProxy/proxy.js
+++ examples/simpleProxy/proxy.js
@@ -1,19 +1,24 @@
 /**
  * This example script demonstrates a basic proxy of the audio and metadata
- * through a Node HTTP server. Invoking this script will start the HTTP server
+ * through a Node HTTP server. The command-line tools `lame` and `oggenc` are
+ * required for this to work. Invoking this script will start the HTTP server
  * on port 36867. If the browser requests:
  *
- *    "/"         - The index page will be returned.
- *    "/stream"   - A radio station will be connected to, and audio data will
- *                  be sent back over to the HTTP client indefinitely.
- *    "/metadata" - A long-polling URI, which will return the value of the
- *                  metadata when a 'metadata' event happens on the
- *                  associated Audio stream.
+ *    "/"           - The index page will be returned.
+ *    "/stream"     - Returns the raw PCM data from the transcoded input radio stream.
+ *    "/stream.mp3" - Returns the radio stream, fed through `lame` and sent to the
+ *                    client as MP3 audio data.
+ *    "/stream.ogg" - Returns the radio stream, fed through `oggenc` and sent to
+ *                    the client as OGG Vorbis audio data.
+ *    "/metadata"   - A long-polling URI, which will return the value of the
+ *                    metadata when a 'metadata' event happens on the
+ *                    associated radio stream.
  *   Usage:
  *     node examples/proxy.js [URL of Icecast Stream to proxy]
  */
 var fs = require("fs");
 var http = require("http");
+var spawn = require("child_process").spawn;
 var radio = require("../../lib/radio-stream");
 
 // If you pass a URL to a SHOUTcast/Icecast stream, then we'll use that,
@@ -25,9 +30,27 @@ console.error("Connecting to: " + station);
 // client requesting the "/stream" URL (will be an <audio> tag).
 var stream = radio.createReadStream(station);
 
+// Decode the MP3 stream to raw PCM data, signed 16-bit Little-endian
+var pcm = spawn("lame", [
+  "-S", // Operate silently (nothing to stderr)
+  "--mp3input", // Decode the MP3 input
+  "-", // Input from stdin
+  "--decode",
+  "-t", // Don't include WAV header info (i.e. output raw PCM)
+  "-s", "48", // Sampling rate: 48,000
+  "--bitwidth", "16", // Bits per Sample: 16
+  "--signed", "--little-endian", // Signed, little-endian samples
+  "-" // Output to stderr
+]);
+stream.on("data", function(chunk) {
+  pcm.stdin.write(chunk);
+});
+
 // Now we create the HTTP server.
 var httpPort = 36867;
 http.createServer(function(req, res) {
+
+  // If the client simple requests 'stream', then send back the raw PCM data.
   if (req.url == "/stream") {
     var connected = function() {
       var headers = {};
@@ -39,10 +62,10 @@ http.createServer(function(req, res) {
       var callback = function(chunk) {
         res.write(chunk);
       }
-      stream.on("data", callback);
+      pcm.stdout.on("data", callback);
       req.connection.on("close", function() {
         // This occurs when the HTTP client closes the connection.
-        stream.removeListener("data", callback);
+        pcm.stdout.removeListener("data", callback);
       });      
     }
     if (stream.connected) {
@@ -50,7 +73,57 @@ http.createServer(function(req, res) {
     } else {
       stream.on("connect", connected);
     }
+
+  } else if (req.url == "/stream.mp3") {
+      res.writeHead(200, {
+        "Content-Type": "audio/mpeg",
+        "Connection": "close",
+        "Transfer-Encoding": "identity"
+      });
+      var mp3 = spawn("lame", [
+        "-S", "-r", "-s", "48", "-", "-"
+      ]);
+      var callback = function(chunk) {
+        mp3.stdin.write(chunk);
+      }
+      mp3.stdout.on("data", function(chunk) {
+        res.write(chunk);
+      });
+      pcm.stdout.on("data", callback);
+      req.connection.on("close", function() {
+        // This occurs when the HTTP client closes the connection.
+        pcm.stdout.removeListener("data", callback);
+        mp3.kill();
+      });      
+ 
+  } else if (req.url == "/stream.ogg") {
+      res.writeHead(200, {
+        "Content-Type": "application/ogg",
+        "Connection": "close",
+        "Transfer-Encoding": "identity"
+      });
+      var ogg = spawn("oggenc", [
+        "--silent", // Silent operation
+        "-r", // Raw input
+        "--ignorelength", // Ignore length
+        "--raw-rate=48000", // Raw input rate: 48000
+        "-" // Input from stdin, Output to stderr
+      ]);
+      var callback = function(chunk) {
+        ogg.stdin.write(chunk);
+      }
+      ogg.stdout.on("data", function(chunk) {
+        res.write(chunk);
+      });
+      pcm.stdout.on("data", callback);
+      req.connection.on("close", function() {
+        // This occurs when the HTTP client closes the connection.
+        pcm.stdout.removeListener("data", callback);
+        ogg.kill();
+      });      
+ 
   } else if (req.url == "/metadata") {
+    req.connection.setTimeout(0); // Disable timeouts
     var callback = function(metadata) {
       stream.removeListener("metadata", callback);
       res.writeHead(200, {
@@ -71,3 +144,4 @@ http.createServer(function(req, res) {
 }).listen(httpPort, function() {
   console.error("HTTP server started on port: " + httpPort);
 });
+
