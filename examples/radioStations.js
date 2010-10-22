e7a219a92fc884ade17bd7a9d015e2251c831f83
Nathan Rajlich <nathan@tootallnate.net>
2010-10-21 22:05:56 -0700

    Merge 'radioStations.json' into 'radioStations.js', and add a `stations.fromName()` function.

--- examples/radioStations.js
+++ examples/radioStations.js
@@ -1,4 +1,8 @@
-exports.stations = JSON.parse(require("fs").readFileSync(__filename+"on"));
+exports.stations = {
+  "4 Ever Floyd": "http://67.205.85.183:7714",
+  "KFRC": "http://2133.live.streamtheworld.com:80/KFRCFMCMP3",
+  ".977 The Comedy Channel": "http://icecast3.977music.com/comedy"
+};
 
 exports.random = function() {
   var keys = Object.keys(exports.stations);
@@ -6,5 +10,17 @@ exports.random = function() {
   return {
     name: key,
     url: exports.stations[key]
+  };
+}
+
+exports.fromName = function(name) {
+  var regexp = new RegExp(name, "i");
+  for (var i in exports.stations) {
+    if (regexp.test(i)) {
+      return {
+        name: i,
+        url: exports.stations[i]
+      };
+    }
   }
 }
