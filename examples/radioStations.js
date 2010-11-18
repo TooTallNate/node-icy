1d2f1d96b3209af06a36ee0539025a295d6610dc
Nathan Rajlich <nathan@tootallnate.net>
2010-11-17 20:05:30 -0800

    Added another test radio station "Feeling Floyd".

--- examples/radioStations.js
+++ examples/radioStations.js
@@ -1,5 +1,6 @@
 exports.stations = {
   "4 Ever Floyd": "http://67.205.85.183:7714",
+  "Feeling Floyd": "http://Streaming23.radionomy.com/FeelingFloyd",
   "KFRC": "http://2133.live.streamtheworld.com:80/KFRCFMCMP3",
   ".977 The Comedy Channel": "http://icecast3.977music.com/comedy"
 };
