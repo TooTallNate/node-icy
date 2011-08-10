74835dc99d0daaf4942c768e8700f7171753be83
Nathan Rajlich <nathan@tootallnate.net>
2011-08-09 23:54:54 -0700

    Fix FeelinFloyd example url. This will probably break like tomorrow, haha

--- examples/radioStations.js
+++ examples/radioStations.js
@@ -1,7 +1,7 @@
 exports.stations = {
   "4 Ever Floyd": "http://67.205.85.183:5500",
   "4 Ever Zeppelin": "http://173.236.59.82:8330",
-  "Feeling Floyd": "http://Streaming23.radionomy.com/FeelingFloyd",
+  "Feeling Floyd": "http://streaming201.radionomy.com:80/FeelingFloyd?group=999",
   "KFRC": "http://2133.live.streamtheworld.com:80/KFRCFMCMP3",
   ".977 The Comedy Channel": "http://icecast3.977music.com/comedy"
 };
