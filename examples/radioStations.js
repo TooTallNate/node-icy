046b4559d1268986eb0bd4e29e4d147c276d4308
Nathan Rajlich <nathan@tootallnate.net>
2011-10-19 17:06:24 -0700

    " -> '

--- examples/radioStations.js
+++ examples/radioStations.js
@@ -1,10 +1,10 @@
 exports.stations = {
-  "4 Ever Floyd": "http://67.205.85.183:5500",
-  "4 Ever Zeppelin": "http://173.236.59.82:8330",
-  "Feeling Floyd": "http://streaming201.radionomy.com:80/FeelingFloyd?group=999",
-  "KFRC": "http://2133.live.streamtheworld.com:80/KFRCFMCMP3",
-  ".977 The Comedy Channel": "http://icecast3.977music.com/comedy",
-  "LivePhish": 'http://radio.nugs.net:8002'
+  '4 Ever Floyd': 'http://67.205.85.183:5500',
+  '4 Ever Zeppelin': 'http://173.236.59.82:8330',
+  'Feeling Floyd': 'http://streaming201.radionomy.com:80/FeelingFloyd?group=999',
+  'KFRC': 'http://2133.live.streamtheworld.com:80/KFRCFMCMP3',
+  '.977 The Comedy Channel': 'http://icecast3.977music.com/comedy',
+  'LivePhish': 'http://radio.nugs.net:8002'
 };
 
 exports.random = function() {
@@ -17,7 +17,7 @@ exports.random = function() {
 }
 
 exports.fromName = function(name) {
-  var regexp = new RegExp(name, "i");
+  var regexp = new RegExp(name, 'i');
   for (var i in exports.stations) {
     if (regexp.test(i)) {
       return {
