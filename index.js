d22740a30cb3b3ca0502908dfdd4aa6c506c7a88
Nathan Rajlich <nathan@tootallnate.net>
2012-10-07 18:25:55 -0700

    index: add some comments

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 
+/**
+ * Parse and stringify metadata Buffers to/from formatted Objects.
+ */
+
 exports.parse = require('./lib/parse');
 exports.stringify = require('./lib/stringify');
 
+/**
+ * Low level transformation classes.
+ */
+
 exports.Reader = require('./lib/reader');
 exports.Writer = require('./lib/writer');
+
+/**
+ * Icecast HTTP client interface. Aims to map directly 1-1 the "http" module's
+ * client interface.
+ */
 
 exports.Client = require('./lib/client');
