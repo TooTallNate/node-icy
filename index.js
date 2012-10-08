d634ef35128d37282130a47326b776b81d6be193
Nathan Rajlich <nathan@tootallnate.net>
2012-10-07 17:59:05 -0700

    implement the Client class
    Leverages node's core `http.ClientRequest` like a fucking badass
    (though in a pretty hacky way...)

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 
 exports.parse = require('./lib/parse');
 exports.stringify = require('./lib/stringify');
+
 exports.Reader = require('./lib/reader');
 exports.Writer = require('./lib/writer');
+
+exports.Client = require('./lib/client');
