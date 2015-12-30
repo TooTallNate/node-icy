796997b07b9ace72968bbdaa2d48847e7f0cd0e1
Angel Nunez Mencias <ANUNEZ@de.ibm.com>
2015-12-30 19:08:20 +0100

    Add test for https

--- test/client.js
+++ test/client.js
@@ -5,6 +5,7 @@
 
 var fs = require('fs');
 var net = require('net');
+var tls = require('tls');
 var path = require('path');
 var icy = require('../');
 var assert = require('assert');
@@ -64,4 +65,62 @@ describe('Client', function () {
       });
     });
   });
+  describe('fixtures: icy-server-https-response', function () {
+      it('should not get a Parse Error with https server', function (done) {
+        var options = {
+            key: fs.readFileSync('test/fixtures/key.pem'),
+            cert: fs.readFileSync('test/fixtures/cert.pem')
+        };
+        //Self signed certificate
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+        var server = tls.createServer(options, function (socket) {
+          var file = path.resolve(__dirname, 'fixtures', 'icy-server-response');
+          fs.createReadStream(file).pipe(socket);
+        });
+        server.listen(function () {
+          var req = icy.request({
+            method: 'GET',
+            protocol: 'https:',
+            host: '127.0.0.1',
+            port: server.address().port,
+            path: '/'
+          }, function (res) {
+            assert.equal('ICY', res.httpVersion);
+            assert.equal('192', res.headers['icy-br']);
+            assert.equal('1', res.headers['icy-pub']);
+            res.resume();
+            server.close();
+            done();
+          });
+          req.end();
+        });
+      });
+    });
+
+  describe('fixtures: icy-server-https-string-response', function () {
+      it('should not get a Parse Error with https server', function (done) {
+        var options = {
+            key: fs.readFileSync('test/fixtures/key.pem'),
+            cert: fs.readFileSync('test/fixtures/cert.pem')
+        };
+        //Self signed certificate
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+        var server = tls.createServer(options, function (socket) {
+          var file = path.resolve(__dirname, 'fixtures', 'icy-server-response');
+          fs.createReadStream(file).pipe(socket);
+        });
+        server.listen(function () {
+          var req = icy.request('https://127.0.0.1:'+server.address().port,
+                                function (res) {
+            assert.equal('ICY', res.httpVersion);
+            assert.equal('192', res.headers['icy-br']);
+            assert.equal('1', res.headers['icy-pub']);
+            res.resume();
+            server.close();
+            done();
+          });
+          req.end();
+        });
+      });
+    });
 });
